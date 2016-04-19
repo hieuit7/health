7abf1cc2cf3ac76ba355332b701d4ea441fd4efa
= <=>
2016-04-19 17:05:54 +0200

    add testcase

--- test/test.js
+++ test/test.js
@@ -66,7 +66,6 @@ describe('service', function() {
       var whois = service.whois("google.tv");
       return whois.then((data) => {
         expect(data).to.deep.equal(
-
           {
             "ip": [{
               "address": "216.58.214.228",
@@ -109,13 +108,351 @@ describe('service', function() {
       }
     });
   });
-  describe("check url", function() {
-    it("check url without compare", function() {
-      var compare = service.compare("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626&key=AIzaSyC7wTia4jUry-Cx_94RQtHhmjdJb0P_q-g");
-      return compare.then((data) => {
-        expect(data).to.deep.equal(
 
-          {
+  describe("check url", function() {
+    it("check url with comparision", function() {
+      var compare = service.compare("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626&key=AIzaSyC7wTia4jUry-Cx_94RQtHhmjdJb0P_q-g", {
+
+        "destination_addresses": [
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "334-350 Hicks St, Brooklyn, NY 11201, USA",
+          "543-633 Dr Wesley McDonald Ave, Brooklyn, NY 11203, USA",
+          "66-0-66-26 103rd St, Rego Park, NY 11374, USA",
+          "925-1001 N Village Ave, Rockville Centre, NY 11570, USA",
+          "301-327 Beach 19th St, Far Rockaway, NY 11691, USA",
+          "543-633 Dr Wesley McDonald Ave, Brooklyn, NY 11203, USA",
+          "66-0-66-26 103rd St, Rego Park, NY 11374, USA",
+          "925-1001 N Village Ave, Rockville Centre, NY 11570, USA",
+          "301-327 Beach 19th St, Far Rockaway, NY 11691, USA"
+        ],
+        "origin_addresses": [
+          "565-569 Vermont St, Brooklyn, NY 11207, USA"
+        ],
+        "rows": [{
+          "elements": [{
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "6.5 mi",
+              "value": 10410
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1957
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "2.9 mi",
+              "value": 4662
+            },
+            "duration": {
+              "text": "17 mins",
+              "value": 1011
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "8.5 mi",
+              "value": 13739
+            },
+            "duration": {
+              "text": "23 mins",
+              "value": 1351
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "15.8 mi",
+              "value": 25440
+            },
+            "duration": {
+              "text": "29 mins",
+              "value": 1711
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "13.2 mi",
+              "value": 21297
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1951
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "2.9 mi",
+              "value": 4662
+            },
+            "duration": {
+              "text": "17 mins",
+              "value": 1011
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "8.5 mi",
+              "value": 13739
+            },
+            "duration": {
+              "text": "23 mins",
+              "value": 1351
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "15.8 mi",
+              "value": 25440
+            },
+            "duration": {
+              "text": "29 mins",
+              "value": 1711
+            },
+            "status": "OK"
+          }, {
+            "distance": {
+              "text": "13.2 mi",
+              "value": 21297
+            },
+            "duration": {
+              "text": "33 mins",
+              "value": 1951
+            },
+            "status": "OK"
+          }]
+        }],
+        "status": "OK"
+      });
+      return compare.then((data) => {
+        expect(data).to.deep.equal({
+          "equal": false,
+          "data": {
+            "destination_addresses": [
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "334-350 Hicks St, Brooklyn, NY 11201, USA",
+              "543-633 Dr Wesley McDonald Ave, Brooklyn, NY 11203, USA",
+              "66-0-66-26 103rd St, Rego Park, NY 11374, USA",
+              "925-1001 N Village Ave, Rockville Centre, NY 11570, USA",
+              "301-327 Beach 19th St, Far Rockaway, NY 11691, USA",
+              "543-633 Dr Wesley McDonald Ave, Brooklyn, NY 11203, USA",
+              "66-0-66-26 103rd St, Rego Park, NY 11374, USA",
+              "925-1001 N Village Ave, Rockville Centre, NY 11570, USA",
+              "301-327 Beach 19th St, Far Rockaway, NY 11691, USA"
+            ],
+            "origin_addresses": [
+              "565-569 Vermont St, Brooklyn, NY 11207, USA"
+            ],
+            "rows": [{
+              "elements": [{
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "6.5 mi",
+                  "value": 10410
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1957
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "2.9 mi",
+                  "value": 4662
+                },
+                "duration": {
+                  "text": "17 mins",
+                  "value": 1011
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "8.5 mi",
+                  "value": 13739
+                },
+                "duration": {
+                  "text": "23 mins",
+                  "value": 1351
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "15.8 mi",
+                  "value": 25440
+                },
+                "duration": {
+                  "text": "29 mins",
+                  "value": 1711
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "13.2 mi",
+                  "value": 21297
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1951
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "2.9 mi",
+                  "value": 4662
+                },
+                "duration": {
+                  "text": "17 mins",
+                  "value": 1011
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "8.5 mi",
+                  "value": 13739
+                },
+                "duration": {
+                  "text": "23 mins",
+                  "value": 1351
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "15.8 mi",
+                  "value": 25440
+                },
+                "duration": {
+                  "text": "29 mins",
+                  "value": 1711
+                },
+                "status": "OK"
+              }, {
+                "distance": {
+                  "text": "13.2 mi",
+                  "value": 21297
+                },
+                "duration": {
+                  "text": "33 mins",
+                  "value": 1951
+                },
+                "status": "OK"
+              }]
+            }],
+            "status": "OK"
+          }
+        })
+      });
+    });
+    it("check url without comparion", function() {
+      var compare = service.compare("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.6905615%2C-73.9976592|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626|40.659569%2C-73.933783|40.729029%2C-73.851524|40.6860072%2C-73.6334271|40.598566%2C-73.7527626&key=AIzaSyC7wTia4jUry-Cx_94RQtHhmjdJb0P_q-g");
+
+      return compare.then((data) => {
+        expect(data).to.deep.equal({
             "equal": false,
             "data": {
               "destination_addresses": [
